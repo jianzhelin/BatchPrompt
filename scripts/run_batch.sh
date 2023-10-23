7708b51a302ad990f6d3beb2dcf9d4b95405e695
Jianzhe Lin <jianzhelin@gmail.com>
2023-10-22 23:58:12 -0700

    Update run_batch.sh

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -1,1 +1,23 @@
-a
+#!/bin/bash
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-chatgpt' --dataset='boolq' --batch_size=32 --num_vote=9 --early_stop=10 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='boolq' --batch_size=32 --num_vote=9 --early_stop=10 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='boolq' --batch_size=64 --num_vote=9 --early_stop=5 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-chatgpt' --dataset='qqp' --batch_size=32 --num_vote=9 --early_stop=10 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='qqp' --batch_size=32 --num_vote=9 --early_stop=10 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='qqp' --batch_size=64 --num_vote=9 --early_stop=5 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='qqp' --batch_size=160 --num_vote=9 --early_stop=2 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-chatgpt' --dataset='rte' --batch_size=32 --num_vote=9 --early_stop=10 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='rte' --batch_size=32 --num_vote=9 --early_stop=10 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='rte' --batch_size=64 --num_vote=9 --early_stop=5 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
+
+python adaptive_batch_run.py --source='aiarch' --engine='aiarch-gpt-4-32k' --dataset='rte' --batch_size=160 --num_vote=9 --early_stop=2 --json_save=True --load_json=True --weight --weights=5 --confidence_type='single'
